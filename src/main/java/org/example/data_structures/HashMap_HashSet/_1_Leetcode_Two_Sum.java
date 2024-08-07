3077b13e9c315c6cfd7488e0d07f6d9f944de48b
DinhVanNhan <vannhan10102004@gmail.com>
2024-08-07 23:27:03 +0700

    Leetcode: 1.Two Sum(Use a hash map.)

---
+++ src/main/java/org/example/data_structures/HashMap_HashSet/_1_Leetcode_Two_Sum.java
@@ -0,0 +1,34 @@
+package org.example.data_structures.HashMap_HashSet;
+
+import java.util.HashMap;
+
+/**
+ * _1_Leetcode_Two_Sum
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date:  8/7/2024
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 8/7/2024    NhanDinhVan    Create
+ */
+
+public class _1_Leetcode_Two_Sum {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++)
+            map.put(nums[i], i);
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(map.containsKey(target - nums[i]) && map.get(target - nums[i]) != i)
+                return new int[] {i , map.get(target - nums[i])};
+        }
+        return new int[] {0, 0};
+
+    }
+}
