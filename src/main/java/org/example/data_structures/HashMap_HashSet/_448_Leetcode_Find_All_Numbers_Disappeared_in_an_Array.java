9f5b15c8a9d3092141d69106f58b9337204ff016
NhanDinhVan <vannhan2409@gmail.com>
2024-08-19 11:54:19 +0700

    Leetcode: 448.Find All Numbers Disappeared in an Array

---
+++ src/main/java/org/example/data_structures/HashMap_HashSet/_448_Leetcode_Find_All_Numbers_Disappeared_in_an_Array.java
@@ -0,0 +1,37 @@
+package org.example.data_structures.HashMap_HashSet;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * _448_Leetcode_Find_All_Numbers_Disappeared_in_an_Array
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date:  8/19/24
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 8/19/24   NhanDinhVan    Create
+ */
+public class _448_Leetcode_Find_All_Numbers_Disappeared_in_an_Array {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+//        ArrayList<Integer> result = new ArrayList<>();
+//        HashSet<Integer> set = new HashSet();
+//        for(int i : nums){
+//            set.add(i);
+//        }
+//        for(int i = 1; i <= nums.length; i++){
+//            if(!set.contains(i))
+//                result.add(i);
+//        }
+//        return result;
+
+        // Solution 2
+
+    }
+}
