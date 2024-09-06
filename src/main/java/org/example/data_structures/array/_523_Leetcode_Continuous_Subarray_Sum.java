73645fe4875ed00a3b42ca97b38b032695fabdd6
NhanDinhVan <vannhan10102004@gmail.com>
2024-09-06 19:59:19 +0700

    Leetcode: 523. Continuous Subarray Sum

---
+++ src/main/java/org/example/data_structures/array/_523_Leetcode_Continuous_Subarray_Sum.java
@@ -0,0 +1,34 @@
+package org.example.data_structures.array;
+
+/**
+ * _523_Leetcode_Continuous_Subarray_Sum
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date:  9/6/24
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 9/6/24   NhanDinhVan    Create
+ */
+public class _523_Leetcode_Continuous_Subarray_Sum {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        // Solution 1: Brute Force (Time Limit Exceeded)
+        // int sum = 0;
+        // for(int i = 0 ; i < nums.length; i++){
+        //     sum = nums[i];
+        //     for(int j = i + 1; j < nums.length; j++){
+        //         sum += nums[j];
+        //         if(sum % k == 0){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+
+    }
+}
