765d4d797fe9f8cfed209b863eb7bee420391291
NhanDinhVan <vannhan10102004@gmail.com>
2024-09-06 12:36:19 +0700

    Leetcode: 137.Single Number II (Solution: Brute Force)

---
+++ src/main/java/org/example/data_structures/HashMap_HashSet/_137_Leetcode_Single_Number_II.java
@@ -0,0 +1,36 @@
+package org.example.data_structures.HashMap_HashSet;
+
+import java.util.Arrays;
+
+/**
+ * _137_Leetcode_Single_Number_II
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date:  9/6/24
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 9/6/24   NhanDinhVan    Create
+ */
+public class _137_Leetcode_Single_Number_II {
+    public int singleNumber(int[] nums) {
+        //Brute Force
+//        boolean isSingle = false;
+//        for(int i = 0 ; i < nums.length ; i++){
+//            isSingle = true;
+//            for(int j = 0 ; j < nums.length; j++){
+//                if(j != i && nums[j] == nums[i])
+//                    isSingle = false;
+//            }
+//            if(isSingle)
+//                return nums[i];
+//        }
+//        return 0;
+
+
+    }
+}
