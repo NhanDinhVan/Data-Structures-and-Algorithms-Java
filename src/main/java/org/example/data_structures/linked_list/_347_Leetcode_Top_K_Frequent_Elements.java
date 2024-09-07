7876af079c716ad9f57f6f44eb67edb0a2fed7f9
NhanDinhVan <vannhan10102004@gmail.com>
2024-09-07 19:18:03 +0700

    Leetcode: 347.Top K Frequent Elements

---
+++ src/main/java/org/example/data_structures/linked_list/_347_Leetcode_Top_K_Frequent_Elements.java
@@ -0,0 +1,50 @@
+package org.example.data_structures.linked_list;
+
+/**
+ * _347_Leetcode_Top_K_Frequent_Elements
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date:  9/7/24
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 9/7/24   NhanDinhVan    Create
+ */
+public class _347_Leetcode_Top_K_Frequent_Elements {
+    public int[] topKFrequent(int[] nums, int k) {
+        // HashMap<Integer, Integer> hsmap = new HashMap<>();
+        // for(int num : nums){
+        //     hsmap.put(num, hsmap.getOrDefault(num, 0)+1);
+        // }
+        // int maxCount;
+        // int max = 0;
+        // int[] result = new int[nums.length];
+        // int count = 0;
+        // while(k > 0){
+        //     max = 0;
+        //     maxCount = 0;
+        //     for (Map.Entry<Integer, Integer> entry : hsmap.entrySet()) {
+        //         if(entry.getValue()>maxCount){
+        //             maxCount = entry.getValue();
+        //             max = entry.getKey();
+        //         }
+        //     }
+        //     result[count++] = max;
+        //     hsmap.remove(max);
+        //     k--;
+        // }
+        // int[] finalResult = new int[count];
+        // for(int i = 0; i < count; i++){
+        //     finalResult[i] = result[i];
+        // }
+        // return finalResult ;
+
+
+
+
+    }
+}
