fca90ba6b20945058945ad98fde0346d6e92f119
NhanDinhVan <vannhan10102004@gmail.com>
2024-09-08 10:43:49 +0700

    Leetcode: 1.Two Sum: Brute Force

--- src/main/java/org/example/data_structures/HashMap_HashSet/_1_Leetcode_Two_Sum.java
+++ src/main/java/org/example/data_structures/HashMap_HashSet/_1_Leetcode_Two_Sum.java
@@ -19,16 +19,25 @@ import java.util.HashMap;
 
 public class _1_Leetcode_Two_Sum {
     public int[] twoSum(int[] nums, int target) {
+//
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        for(int i = 0; i < nums.length; i++)
+//            map.put(nums[i], i);
+//        for(int i = 0; i < nums.length; i++)
+//        {
+//            if(map.containsKey(target - nums[i]) && map.get(target - nums[i]) != i)
+//                return new int[] {i , map.get(target - nums[i])};
+//        }
+//        return new int[] {0, 0};
+//      Solution: Brute Force
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++)
-            map.put(nums[i], i);
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(map.containsKey(target - nums[i]) && map.get(target - nums[i]) != i)
-                return new int[] {i , map.get(target - nums[i])};
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length ; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[] {i, j};
+                }
+            }
         }
-        return new int[] {0, 0};
-
+        return new int[] {-1,-1};
     }
 }
