1f4621c3b55f767f57ed8c89415a9b25c58a87bf
NhanDinhVan <vannhan10102004@gmail.com>
2024-09-21 20:52:49 +0700

    Leetcode: 231. Power of Two. (Bit manipulation.)

--- src/main/java/org/example/data_structures/tree/_231_Leetcode_Power_of_Two.java
+++ src/main/java/org/example/data_structures/tree/_231_Leetcode_Power_of_Two.java
@@ -16,13 +16,14 @@ package org.example.data_structures.tree;
  */
 public class _231_Leetcode_Power_of_Two {
     public boolean isPowerOfTwo(int n) {
-        if(n <= 0)
-            return false;
-        if(n%2 == 0)
-            return isPowerOfTwo(n/2);
-        if(n == 1)
-            return true;
-        else
-            return false;
+//        if(n <= 0)
+//            return false;
+//        if(n%2 == 0)
+//            return isPowerOfTwo(n/2);
+//        if(n == 1)
+//            return true;
+//        else
+//            return false;
+        return n > 0 && (n & (n-1)) == 0;
     }
 }
