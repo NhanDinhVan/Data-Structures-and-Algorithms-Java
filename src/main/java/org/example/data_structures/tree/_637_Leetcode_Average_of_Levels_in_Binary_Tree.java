b81e3fd22fb99fca55431480738b275806aab47b
NhanDinhVan <vannhan10102004@gmail.com>
2024-09-21 10:17:09 +0700

    Leetcode: 637. Average of Levels in Binary Tree. (Beats 26.81%)

---
+++ src/main/java/org/example/data_structures/tree/_637_Leetcode_Average_of_Levels_in_Binary_Tree.java
@@ -0,0 +1,44 @@
+package org.example.data_structures.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * _637_Leetcode_Average_of_Levels_in_Binary_Tree
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date:  9/21/24
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 9/21/24   NhanDinhVan    Create
+ */
+public class _637_Leetcode_Average_of_Levels_in_Binary_Tree {
+    public List<Double> averageOfLevels(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        List<Double> result = new ArrayList<>();
+        while(!q.isEmpty()){
+            int size = q.size();
+            Double sum = 0.0;
+            for(int i = 0; i < size; i++){
+                TreeNode tmp = q.poll();
+                if(tmp.left != null) {
+                    q.add(tmp.left);
+                }
+                if(tmp.right != null) {
+                    q.add(tmp.right);
+                }
+                sum += tmp.val;
+            }
+            result.add((sum/size));
+        }
+        return result;
+    }
+}
