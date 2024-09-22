f99ed18a1248228c59db62da1739967e901a362c
NhanDinhVan <vannhan10102004@gmail.com>
2024-09-22 19:22:33 +0700

    Leetcode: 389. Find the Difference( Clean code)

--- src/main/java/org/example/algorithms/sort_algorithms/_389_Leetcode_Find_the_Difference.java
+++ src/main/java/org/example/algorithms/sort_algorithms/_389_Leetcode_Find_the_Difference.java
@@ -18,21 +18,35 @@ import java.util.Arrays;
  */
 public class _389_Leetcode_Find_the_Difference {
     public char findTheDifference(String s, String t) {
-        char[] sArray = s.toCharArray();
-        Arrays.sort(sArray);
+//        char[] sArray = s.toCharArray();
+//        Arrays.sort(sArray);
+//
+//        char[] tArray = t.toCharArray();
+//        Arrays.sort(tArray);
+//        int i = 0;
+//        int j = 0;
+//        while(i < sArray.length || j < tArray.length){
+//            if(i == sArray.length)
+//                return tArray[j];
+//            if(sArray[i] != tArray[j])
+//                return tArray[j];
+//            i++; j++;
+//        }
+//        return 't';
 
+        char[] sArray = s.toCharArray();
         char[] tArray = t.toCharArray();
+
+        Arrays.sort(sArray);
         Arrays.sort(tArray);
-        int i = 0;
-        int j = 0;
-        while(i < sArray.length || j < tArray.length){
-            if(i == sArray.length)
-                return tArray[j+1];
-            if(sArray[i] != tArray[j])
-                return tArray[j];
-            i++; j++;
+
+        for (int i = 0; i < sArray.length; i++) {
+            if (sArray[i] != tArray[i]) {
+                return tArray[i];
+            }
         }
-        return 't';
+
+        return tArray[tArray.length - 1];
     }
 //    public String quickSort(String s, int l, int r){
 //
