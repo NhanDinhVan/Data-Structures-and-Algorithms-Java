d90eb4021b613b7c9b36d16aab9c46e193d3de3b
NhanDinhVan <vannhan10102004@gmail.com>
2024-09-26 11:00:02 +0700

    Leetcode: 167. Two Sum II - Input Array Is Sorted. (Brute Force - Beats: 5.34%)

--- src/main/java/org/example/data_structures/array/_167_Leetcode_Two_Sum_II_Input_Array_Is_Sorted.java
+++ src/main/java/org/example/data_structures/array/_167_Leetcode_Two_Sum_II_Input_Array_Is_Sorted.java
@@ -2,6 +2,12 @@ package org.example.data_structures.array;
 
 public class _167_Leetcode_Two_Sum_II_Input_Array_Is_Sorted {
     public int[] twoSum(int[] numbers, int target) {
+        for(int i = 0 ; i < numbers.length - 1; i++){
+            for(int j = i+1; j < numbers.length ; j++){
+                if(numbers[i] + numbers[j] == target)
+                    return new int[] {i + 1, j + 1};
+            }
+        }
         return null;
     }
 }
