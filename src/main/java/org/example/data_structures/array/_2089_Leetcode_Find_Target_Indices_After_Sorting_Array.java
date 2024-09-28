8761117d1e11ec7bb95fd1ae79ad67edcba3a3fb
NhanDinhVan <vannhan10102004@gmail.com>
2024-09-28 21:29:46 +0700

    Leetcode: 2089.Find Target Indices After Sorting Array

---
+++ src/main/java/org/example/data_structures/array/_2089_Leetcode_Find_Target_Indices_After_Sorting_Array.java
@@ -0,0 +1,31 @@
+package org.example.data_structures.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * _2089_Leetcode_Find_Target_Indices_After_Sorting_Array
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date:  9/28/24
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 9/28/24   NhanDinhVan    Create
+ */
+public class _2089_Leetcode_Find_Target_Indices_After_Sorting_Array {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target)
+                result.add(nums[i]);
+        }
+        return result;
+    }
+}
