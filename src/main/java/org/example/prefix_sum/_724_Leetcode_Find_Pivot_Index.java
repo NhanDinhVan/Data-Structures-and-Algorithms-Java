0cfc51307f0389bfca23f0fe9d614149ae2e074a
NhanDinhVan <vannhan10102004@gmail.com>
2024-10-05 02:01:34 +0700

    Leetcode: 724. Find Pivot Index

---
+++ src/main/java/org/example/prefix_sum/_724_Leetcode_Find_Pivot_Index.java
@@ -0,0 +1,39 @@
+package org.example.prefix_sum;
+
+/**
+ * _724_Leetcode_Find_Pivot_Index
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date:  10/4/24
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 10/4/24   NhanDinhVan    Create
+ */
+public class _724_Leetcode_Find_Pivot_Index {
+    public int pivotIndex(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        int suml = nums[l], sumr = nums[r];
+        while(l != r){
+            if(suml > sumr){
+                r--;
+                sumr += nums[r];
+            }else if(suml < sumr){
+                l++;
+                suml += nums[l];
+            }else{
+                l++;
+                r--;
+                if(r==l){
+                    return l;
+                }
+            }
+        }
+        return l;
+    }
+}
