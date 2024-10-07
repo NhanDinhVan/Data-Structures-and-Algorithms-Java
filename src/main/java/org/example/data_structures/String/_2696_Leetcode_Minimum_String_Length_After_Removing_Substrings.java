f6ae56c90198350fae273d8d43788939c0d48a59
DinhVanNhan <vannhan10102004@gmail.com>
2024-10-07 11:44:48 +0700

    Leetcode: 2696. Minimum String Length After Removing Substrings(Beats 65.30%)

--- src/main/java/org/example/data_structures/String/_2696_Leetcode_Minimum_String_Length_After_Removing_Substrings.java
+++ src/main/java/org/example/data_structures/String/_2696_Leetcode_Minimum_String_Length_After_Removing_Substrings.java
@@ -1,5 +1,7 @@
 package org.example.data_structures.String;
 
+import java.util.Stack;
+
 /**
  * _2696_Leetcode_Minimum_String_Length_After_Removing_Substrings
  * <p>
@@ -17,20 +19,29 @@ package org.example.data_structures.String;
 
 public class _2696_Leetcode_Minimum_String_Length_After_Removing_Substrings {
     public int minLength(String s) {
-        if(s.length() < 2)
-            return s.length();
-        int l = 0;
-        int r = 1;
-        while(r < s.length()){
-            String s1 = s.substring(l, r);
-            if(s1.equals("AB") || s1.equals("CD")){
-                s = s.substring(0,l) + s.substring(r+1);
-                l = 0;
-                r = 1;
+//        if(s.length() < 2)
+//            return s.length();
+//        int l = 0;
+//        int r = 1;
+//        while(r < s.length()){
+//            String s1 = s.substring(l, r);
+//            if(s1.equals("AB") || s1.equals("CD")){
+//                s = s.substring(0,l) + s.substring(r+1);
+//                l = 0;
+//                r = 1;
+//            }else{
+//                l++; r++;
+//            }
+//        }
+//        return s.length();
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(!stack.isEmpty() && ((stack.peek() == 'A' && s.charAt(i) == 'B') || (stack.peek() == 'C' && s.charAt(i) == 'D'))){
+                stack.pop();
             }else{
-                l++; r++;
+                stack.push(s.charAt(i));
             }
         }
-        return s.length();
+        return stack.size();
     }
 }
