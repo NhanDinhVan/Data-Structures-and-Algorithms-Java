2de87115d738ad393124a71e4130b82fe7912ed2
DinhVanNhan <vannhan10102004@gmail.com>
2024-10-14 20:37:30 +0700

    Leetcode: 1700. Number of Students Unable to Eat Lunch(Two Queues)

--- src/main/java/org/example/data_structures/stack_queue/_1700_Leetcode_Number_of_Students_Unable_to_Eat_Lunch.java
+++ src/main/java/org/example/data_structures/stack_queue/_1700_Leetcode_Number_of_Students_Unable_to_Eat_Lunch.java
@@ -1,5 +1,8 @@
 package org.example.data_structures.stack_queue;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  * _1700_Leetcode_Number_of_Students_Unable_to_Eat_Lunch
  * <p>
@@ -17,26 +20,44 @@ package org.example.data_structures.stack_queue;
 
 public class _1700_Leetcode_Number_of_Students_Unable_to_Eat_Lunch {
     public int countStudents(int[] students, int[] sandwiches) {
-        int circleCount = 0;
-        int squareCount = 0;
-
+//        int circleCount = 0;
+//        int squareCount = 0;
+//
+//        for(int i = 0; i < students.length; i++){
+//            if(students[i] == 1){
+//                squareCount++;
+//            }else{
+//                circleCount++;
+//            }
+//        }
+//        for(int i = 0; i < sandwiches.length; i++){
+//            if(sandwiches[i] == 0 && circleCount == 0)
+//                return squareCount;
+//            if(sandwiches[i] == 1 && squareCount == 0)
+//                return circleCount;
+//            if(sandwiches[i] == 0)
+//                circleCount--;
+//            else
+//                squareCount--;
+//        }
+//        return 0;
+        Queue<Integer> studentQueue = new ArrayDeque<>();
+        Queue<Integer> sandwichQueue = new ArrayDeque<>();
+        int lastServed = 0;
         for(int i = 0; i < students.length; i++){
-            if(students[i] == 1){
-                squareCount++;
-            }else{
-                circleCount++;
+            studentQueue.add(students[i]);
+            sandwichQueue.add(sandwiches[i]);
+        }
+        while(studentQueue.size() > 0 && lastServed < studentQueue.size()){
+            if(sandwichQueue.peek() == studentQueue.peek()){
+                sandwichQueue.poll();
+                studentQueue.poll();
+                lastServed = 0;
+            }else {
+                studentQueue.offer(studentQueue.poll());
+                lastServed++;
             }
         }
-        for(int i = 0; i < sandwiches.length; i++){
-            if(sandwiches[i] == 0 && circleCount == 0)
-                return squareCount;
-            if(sandwiches[i] == 1 && squareCount == 0)
-                return circleCount;
-            if(sandwiches[i] == 0)
-                circleCount--;
-            else
-                squareCount--;
-        }
-        return 0;
+        return studentQueue.size();
     }
 }
