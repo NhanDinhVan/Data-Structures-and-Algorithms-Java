095b816ea9d84d16dbe3ee4e3edda17c9841c5a6
DinhVanNhan <vannhan10102004@gmail.com>
2024-10-27 16:19:22 +0700

    Q2-Weekly-Contest-420: Leetcode: 3325.Count Substrings With K-Frequency Characters I

--- src/main/java/org/example/contests/weekly_contest_420/_Q2_Count_Substrings_With_K_Frequency_Characters_I.java
+++ src/main/java/org/example/contests/weekly_contest_420/_Q2_Count_Substrings_With_K_Frequency_Characters_I.java
@@ -17,6 +17,17 @@ package org.example.contests.weekly_contest_420;
 
 public class _Q2_Count_Substrings_With_K_Frequency_Characters_I {
     public int numberOfSubstrings(String s, int k) {
-        return 0;
+        int count = 0;
+        for(int l = 0; l < s.length(); l++){
+            int[] fre = new int[26];
+            for(int r = l; r <s.length(); r++){
+                fre[s.charAt(r) - 'a']++;
+                if(fre[s.charAt(r) - 'a'] == k){
+                    count += (s.length() - r);
+                    break;
+                }
+            }
+        }
+        return count;
     }
 }
