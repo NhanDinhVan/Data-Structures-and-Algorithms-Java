5ab5605bfde10f911cde45f57651d3a0b9598e35
DinhVanNhan <vannhan10102004@gmail.com>
2024-11-06 19:49:30 +0700

    Leetcode: 90. Subsets II(Backtracking, Decision Tree, HashSet - Beats 23%)

---
+++ src/main/java/org/example/backtracking/_90_Leetcode_Subsets_II.java
@@ -0,0 +1,43 @@
+package org.example.backtracking;
+
+import java.util.*;
+
+/**
+ * _90_Leetcode_Subsets_II
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date:  11/6/2024
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 11/6/2024    NhanDinhVan    Create
+ */
+
+public class _90_Leetcode_Subsets_II {
+    List<List<Integer>> ans = new ArrayList<>();
+    HashSet<List<Integer>> hset = new HashSet<>();
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        backtrack( 0, nums, new ArrayList<>());
+        return ans;
+    }
+
+    public void backtrack(int start, int[] nums, List<Integer> list){
+        if(start == nums.length){
+            if(!hset.contains(list)){
+                ans.add(new ArrayList<>(list));
+            }
+            hset.add(new ArrayList<>(list));
+            return;
+        }
+
+        list.add(nums[start]);
+        backtrack( start + 1, nums, list);
+        list.remove(list.size() - 1);
+        backtrack( start+1, nums, list);
+    }
+}
