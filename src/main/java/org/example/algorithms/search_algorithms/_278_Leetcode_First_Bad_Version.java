6fb6e15f8e83e8622f112f9b4f697fc7107b7986
DinhVanNhan <vannhan10102004@gmail.com>
2024-11-06 16:51:32 +0700

    Leetcode: 278. First Bad Version.

---
+++ src/main/java/org/example/algorithms/search_algorithms/_278_Leetcode_First_Bad_Version.java
@@ -0,0 +1,38 @@
+package org.example.algorithms.search_algorithms;
+
+/**
+ * _278_Leetcode_First_Bad_Version
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date:  11/6/2024
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 11/6/2024    NhanDinhVan    Create
+ */
+
+public class _278_Leetcode_First_Bad_Version {
+    public int firstBadVersion(int n) {
+//        if(n == 1)
+//            return n;
+//        int left = 1;
+//        int right = n;
+//        int firstBadVersion = 1;
+//
+//        while (left <= right){
+//            int mid = left + (right - left)/2;
+//            if(isBadVersion(mid)){
+//                right = mid - 1;
+//                firstBadVersion = mid;
+//            }else{
+//                left = mid + 1;
+//            }
+//        }
+//        return firstBadVersion;
+        return 0;
+    }
+}
