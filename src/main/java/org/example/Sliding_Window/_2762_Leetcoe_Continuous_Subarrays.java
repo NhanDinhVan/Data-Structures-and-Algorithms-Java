605f171c0fe88b62673df1ccbc9c773ad84b00de
NhanDinhVan <vannhan10102004@gmail.com>
2024-12-15 16:05:00 +0700

    Leetcode: 2762. Continuous Subarrays

---
+++ src/main/java/org/example/Sliding_Window/_2762_Leetcoe_Continuous_Subarrays.java
@@ -0,0 +1,29 @@
+package org.example.Sliding_Window;
+
+import javafx.util.Pair;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Set;
+
+public class _2762_Leetcoe_Continuous_Subarrays {
+    public long continuousSubarrays(int[] nums) {
+        if(nums.length == 1) return 1;
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+        long total = 0;
+        int l = 0;
+        int r = 0;
+
+        while(r < nums.length){
+            frequencyMap.put(nums[r], frequencyMap.getOrDefault(nums[r], 0)+1);
+            while(Collections.max(frequencyMap.keySet()) - Collections.min(frequencyMap.keySet()) > 2){
+                frequencyMap.put(nums[l], frequencyMap.get(nums[l]) - 1);
+                if(frequencyMap.get(nums[l]) == 0) frequencyMap.remove(nums[l]);
+                l++;
+            }
+            total += r - l + 1;
+            r++;
+        }
+        return total;
+    }
+}
