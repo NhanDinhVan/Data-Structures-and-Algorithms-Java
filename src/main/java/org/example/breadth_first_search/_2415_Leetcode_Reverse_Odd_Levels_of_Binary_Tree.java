5ff79c895b5765fe849628f638b6aee4b7128a07
NhanDinhVan <vannhan10102004@gmail.com>
2024-12-20 17:37:18 +0700

    Leetcode: 2415. Reverse Odd Levels of Binary Tree - DFS - Beats 100%

--- src/main/java/org/example/breadth_first_search/_2415_Leetcode_Reverse_Odd_Levels_of_Binary_Tree.java
+++ src/main/java/org/example/breadth_first_search/_2415_Leetcode_Reverse_Odd_Levels_of_Binary_Tree.java
@@ -15,34 +15,48 @@ public class _2415_Leetcode_Reverse_Odd_Levels_of_Binary_Tree {
              this.right = right;
          }
      }
+//    public TreeNode reverseOddLevels(TreeNode root) {
+//        Queue<TreeNode> queue = new LinkedList<>();
+//        int level = 0;
+//        queue.add(root);
+//        while(!queue.isEmpty()){
+//            int size = queue.size();
+//            List<TreeNode> currentLevelNodes = new ArrayList<>();
+//
+//            for (int i = 0; i < size; i++) {
+//                TreeNode node = queue.poll();
+//                currentLevelNodes.add(node);
+//                if (node.left != null) queue.add(node.left);
+//                if (node.right != null) queue.add(node.right);
+//            }
+//
+//            if (level % 2 != 0) {
+//                int left = 0, right = currentLevelNodes.size() - 1;
+//                while (left < right) {
+//                    int temp = currentLevelNodes.get(left).val;
+//                    currentLevelNodes.get(left).val = currentLevelNodes.get(right).val;
+//                    currentLevelNodes.get(right).val = temp;
+//                    left++;
+//                    right--;
+//                }
+//            }
+//            level++;
+//        }
+//        return root;
+//    }
     public TreeNode reverseOddLevels(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        int level = 0;
-        queue.add(root);
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            List<TreeNode> currentLevelNodes = new ArrayList<>();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                currentLevelNodes.add(node);
-                if (node.left != null) queue.add(node.left);
-                if (node.right != null) queue.add(node.right);
-            }
-
-            if (level % 2 != 0) {
-                int left = 0, right = currentLevelNodes.size() - 1;
-                while (left < right) {
-                    int temp = currentLevelNodes.get(left).val;
-                    currentLevelNodes.get(left).val = currentLevelNodes.get(right).val;
-                    currentLevelNodes.get(right).val = temp;
-                    left++;
-                    right--;
-                }
-            }
-            level++;
-        }
+        dfs(root.left, root.right, 1);
         return root;
     }
+    public void dfs(TreeNode left, TreeNode right, int level){
+        if(left == null && right == null) return;
 
+        if(level%2!=0){
+            int tmp = left.val;
+            left.val = right.val;
+            right.val = tmp;
+        }
+        dfs(left.left, right.right, level+1);
+        dfs(left.right, right.left, level+1);
+    }
 }
