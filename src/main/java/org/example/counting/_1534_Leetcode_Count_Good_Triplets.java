a4376f0db5165f82676c8b5f7c380e8e183ce75c
DinhVanNhan <vannhan10102004@gmail.com>
2024-12-30 12:34:32 +0700

    Leetcode: 1534. Count Good Triplets - Optimize

--- src/main/java/org/example/counting/_1534_Leetcode_Count_Good_Triplets.java
+++ src/main/java/org/example/counting/_1534_Leetcode_Count_Good_Triplets.java
@@ -19,18 +19,33 @@ import java.util.Map;
 
 public class _1534_Leetcode_Count_Good_Triplets {
     public int countGoodTriplets(int[] arr, int a, int b, int c) {
-        int n = arr.length;
-        int count = 0;
+//        int n = arr.length;
+//        int count = 0;
+//
+//        for(int i = 0; i < n-2; i++){
+//            for(int j = i+1; j < n-1; j++){
+//                for(int k = j+1; k < n; k++){
+//                    if( Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c){
+//                        count++;
+//                    }
+//                }
+//            }
+//        }
+//        return count;
 
-        for(int i = 0; i < n-2; i++){
-            for(int j = i+1; j < n-1; j++){
-                for(int k = j+1; k < n; k++){
-                    if( Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c){
-                        count++;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (Math.abs(arr[i] - arr[j]) <= a) {
+                    for (int k = j + 1; k < arr.length; k++) {
+                        if (Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[k] - arr[i]) <= c) {
+                            count++;
+                        }
                     }
                 }
             }
         }
+
         return count;
     }
 }
