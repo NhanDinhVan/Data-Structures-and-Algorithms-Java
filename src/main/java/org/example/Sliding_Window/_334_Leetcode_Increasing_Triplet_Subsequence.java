b2e02faaebba03ebd13280c2177d559a15d72a30
DinhVanNhan <vannhan10102004@gmail.com>
2024-12-30 12:02:44 +0700

    Leetcode: 334. Increasing Triplet Subsequence - Beats 100%

---
+++ src/main/java/org/example/Sliding_Window/_334_Leetcode_Increasing_Triplet_Subsequence.java
@@ -0,0 +1,36 @@
+package org.example.Sliding_Window;
+
+/**
+ * _334_Leetcode_Increasing_Triplet_Subsequence
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date:  12/30/2024
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 12/30/2024    NhanDinhVan    Create
+ */
+
+public class _334_Leetcode_Increasing_Triplet_Subsequence {
+    public boolean increasingTriplet(int[] nums) {
+        if(nums.length < 3) return false;
+
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for(int i : nums){
+            if(i <= first){
+                first = i;
+            }else if(i <= second){
+                second = i;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
