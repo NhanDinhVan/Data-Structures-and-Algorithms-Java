61fd53049a314892b3245ea124247784339abd46
DinhVanNhan <vannhan10102004@gmail.com>
2025-01-02 13:18:59 +0700

    Leetcode: 922. Sort Array By Parity II - Optimized Solution - Two Pointer

--- src/main/java/org/example/sorting/_922_Leetcode_Sort_Array_By_Parity_II.java
+++ src/main/java/org/example/sorting/_922_Leetcode_Sort_Array_By_Parity_II.java
@@ -18,24 +18,44 @@ import java.util.PriorityQueue;
  */
 
 public class _922_Leetcode_Sort_Array_By_Parity_II {
-    public int[] sortArrayByParityII(int[] nums) {
-        PriorityQueue<Integer> oldQ = new PriorityQueue<>();
-        PriorityQueue<Integer> evenQ = new PriorityQueue<>();
-
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] % 2 == 0){
-                evenQ.add(nums[i]);
-            }else{
-                oldQ.add(nums[i]);
+//    public int[] sortArrayByParityII(int[] nums) {
+//        PriorityQueue<Integer> oldQ = new PriorityQueue<>();
+//        PriorityQueue<Integer> evenQ = new PriorityQueue<>();
+//
+//        for(int i = 0; i < nums.length; i++){
+//            if(nums[i] % 2 == 0){
+//                evenQ.add(nums[i]);
+//            }else{
+//                oldQ.add(nums[i]);
+//            }
+//        }
+//        for(int i = 0; i < nums.length; i++){
+//            if(i%2 == 0){
+//                nums[i] = evenQ.poll();
+//            }else{
+//                nums[i] = oldQ.poll();
+//            }
+//        }
+//        return nums;
+//    }
+    public int[] sortArrayByParityII(int[] A) {
+        int i = 0, j = 1, n = A.length;
+        while (i < n && j < n) {
+            while (i < n && A[i] % 2 == 0) {
+                i += 2;
+            }
+            while (j < n && A[j] % 2 == 1) {
+                j += 2;
+            }
+            if (i < n && j < n) {
+                swap(A, i, j);
             }
         }
-        for(int i = 0; i < nums.length; i++){
-            if(i%2 == 0){
-                nums[i] = evenQ.poll();
-            }else{
-                nums[i] = oldQ.poll();
-            }
-        }
-        return nums;
+        return A;
+    }
+    private void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
     }
 }
