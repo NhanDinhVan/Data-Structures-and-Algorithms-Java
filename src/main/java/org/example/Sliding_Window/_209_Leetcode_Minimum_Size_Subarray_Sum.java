b8ce86d1213134a49184b18b5578f84d7b1f23b6
DinhVanNhan <vannhan10102004@gmail.com>
2025-01-10 21:21:44 +0700

    Leetcode: 209. Minimum Size Subarray Sum

---
+++ src/main/java/org/example/Sliding_Window/_209_Leetcode_Minimum_Size_Subarray_Sum.java
@@ -0,0 +1,38 @@
+package org.example.Sliding_Window;
+
+/**
+ * _209_Leetcode_Minimum_Size_Subarray_Sum
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date:  1/10/2025
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 1/10/2025    NhanDinhVan    Create
+ */
+
+public class _209_Leetcode_Minimum_Size_Subarray_Sum {
+    public int minSubArrayLen(int target, int[] nums) {
+        int minLen = Integer.MAX_VALUE;
+        int left = 0;
+        int curSum = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            curSum += nums[right];
+
+            while (curSum >= target) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                }
+                curSum -= nums[left];
+                left++;
+            }
+        }
+
+        return minLen != Integer.MAX_VALUE ? minLen : 0;
+    }
+}
