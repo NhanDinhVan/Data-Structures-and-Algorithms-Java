554062b238589d2163a57ff62d685d53e1a21229
DinhVanNhan <vannhan10102004@gmail.com>
2025-01-24 20:01:59 +0700

    Leetcode: 2319. Check if Matrix Is X-Matrix

---
+++ src/main/java/org/example/matrix/_2319_Leetcode_Check_if_Matrix_Is_X_Matrix.java
@@ -0,0 +1,32 @@
+package org.example.matrix;
+
+/**
+ * _2319_Leetcode_Check_if_Matrix_Is_X_Matrix
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date:  1/24/2025
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 1/24/2025    NhanDinhVan    Create
+ */
+
+public class _2319_Leetcode_Check_if_Matrix_Is_X_Matrix {
+    public boolean checkXMatrix(int[][] grid) {
+        int n = grid.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1) {
+                    if (grid[i][j] == 0) return false;
+                } else {
+                    if (grid[i][j] != 0) return false;
+                }
+            }
+        }
+        return true;
+    }
+}
