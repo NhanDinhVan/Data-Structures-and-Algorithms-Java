d0014b037893849eef573a33bd21f0dad1efb946
DinhVanNhan <vannhan10102004@gmail.com>
2025-03-09 13:20:48 +0700

    Leetcode Weekly Contest 440: Q1. Fruits Into Baskets II

---
+++ src/main/java/org/example/contests/weekly_contest_440/_Q1_Fruits_Into_Baskets_II.java
@@ -0,0 +1,39 @@
+package org.example.contests.weekly_contest_440;
+
+/**
+ * _Q1_Fruits_Into_Baskets_II
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date:  3/9/2025
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 3/9/2025    NhanDinhVan    Create
+ */
+
+public class _Q1_Fruits_Into_Baskets_II {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = baskets.length;
+        boolean[] used = new boolean[n];
+        int unplaced = 0;
+
+        for (int fruit : fruits) {
+            boolean placed = false;
+            for (int j = 0; j < n; j++) {
+                if (baskets[j] >= fruit && !used[j]) {
+                    used[j] = true;
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) {
+                unplaced++;
+            }
+        }
+        return unplaced;
+    }©leetcode
+}
