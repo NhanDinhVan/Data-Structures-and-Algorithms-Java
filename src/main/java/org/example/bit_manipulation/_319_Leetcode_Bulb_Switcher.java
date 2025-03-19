230366c23d47dc027ed701c0c7927416aa1a479f
DinhVanNhan <vannhan10102004@gmail.com>
2025-03-19 23:49:13 +0700

    Leetcode: 319. Bulb Switcher.

---
+++ src/main/java/org/example/bit_manipulation/_319_Leetcode_Bulb_Switcher.java
@@ -0,0 +1,22 @@
+package org.example.bit_manipulation;
+
+/**
+ * _319_Leetcode_Bulb_Switcher
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date:  3/19/2025
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 3/19/2025    NhanDinhVan    Create
+ */
+
+public class _319_Leetcode_Bulb_Switcher {
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
